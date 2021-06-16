24e4668fc8c9f29fcff5900eb4cf06083d3e41e8
nebocco <nebocco@gmail.com>
2021-06-16 16:28:18 +0900

    add: heroku-ssl-redirect

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
+import express from 'express';
+import path from 'path';
+import serveStatic from 'serve-static';
+import sslRedirect from 'heroku-ssl-redirect'
+
+const app = express();
+app.use(sslRedirect())
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
